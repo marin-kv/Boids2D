366b342dd85bbb8b6b9226c71ece30cd953d1f09
marin-kv <marinkovacevicvranjican@gmail.com>
2021-04-22 21:52:26 +0200

    Add files

---
+++ Assets/Behaviour Scripts/AlignmentBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Alignment")]
+public class AlignmentBehaviour : FilteredFlockBehaviour
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (context.Count == 0)
+        {
+            return agent.transform.up;
+        }
+
+        Vector2 alignmentMove = Vector2.zero;
+        List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+        foreach (Transform item in filteredContext)
+        {
+            alignmentMove += (Vector2)item.transform.up;
+        }
+        alignmentMove /= context.Count;
+
+        return alignmentMove;
+    }
+}
---
+++ Assets/Behaviour Scripts/CohesionBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Cohesion")]
+public class CohesionBehaviour : FilteredFlockBehaviour
+{
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        Vector2 cohesionMove = Vector2.zero;
+        List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+        foreach (Transform item in filteredContext)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+        cohesionMove /= context.Count;
+
+        cohesionMove -= (Vector2)agent.transform.position;
+        return cohesionMove;
+    }
+}
